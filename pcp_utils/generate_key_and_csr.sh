3c8aee8d846f9e13c36cee03788519a8d18036b9
Simon Ott <simon.ott@aisec.fraunhofer.de>
2023-02-12 22:42:51 +0100

    pcp_utils/generate_key_and_csr.sh: Fixed usage check

--- pcp_utils/generate_key_and_csr.sh
+++ pcp_utils/generate_key_and_csr.sh
@@ -14,15 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This script triggers the generation of a private-public key pair and a respective CSR. 
+# This script triggers the generation of a private-public key pair and a respective CSR.
 # It expects the directory used for the pki, the intended profile and the subject name for the intended certificate (without whitespaces)
 if [ "$#" != "3" ] ; then
 	echo "Usage: ./generate_key_and_csr.sh <pki_dir> [developer|evaluator|certifier|operator|device] <common name>"
 	exit 1
 fi
 
-if [ "$2" != "developer" ] && [ "$2" != "evaluator" ] && [ "$2" = "certifier" ] && [ "$2" = "operator" ] && [ "$2" = "device" ]; then
-	echo "Usage: ./generate_key_and_csr.sh <pki_dir>  [developer|evaluator|certifier|operator|device] <common name>"
+if [ "$2" != "developer" ] && [ "$2" != "evaluator" ] && [ "$2" != "certifier" ] && [ "$2" != "operator" ] && [ "$2" != "device" ]; then
+	echo "Usage: ./generate_key_and_csr.sh <pki_dir> [developer|evaluator|certifier|operator|device] <common name>"
 	exit 1
 fi
 
